e28462fd95ce9f87e03b25c5c71433e7edaa0834
Chang Shu Glasgow <2357702s@student.gla.ac.uk>
2021-08-05 16:46:39 +0100

    refactor data preparing

--- data_prep.py
+++ data_prep.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import numpy as np
 from scipy.io import loadmat
 import os
@@ -5,45 +7,13 @@ from provider import shuffle_data, sliding_window
 from tqdm import tqdm
 
 
-def make_discrete_data(data_path, train_dir, test_dir):
-    """
-    """
-    data = loadmat(data_path)['Doppler']
-    label = loadmat(data_path)['Label']
-
-    data_x, data_y = [[] for _ in range(2)]
-    data_size = len(data)
-    for i in tqdm(range(data_size)):
-        raw_x = data[i][0].T
-        seq_len, feat_dims = raw_x.shape
-        dop_x = np.abs(raw_x) # extract doppler
-        raw_y = np.zeros([seq_len, 1], dtype=np.int32)
-        raw_y[:, :] = label[i][0] - 1 # convert the indexing format
-        slide_x, slide_y = sliding_window(dop_x, raw_y, overlap=0.9, win_len=50)
-        data_x.append(slide_x)
-        data_y.append(slide_y)
-    data_x = np.vstack(data_x)
-    data_y = np.vstack(data_y) # (data_size, seq_len, feat_dims)
-
-    # train test split
-    shuffled_x, shuffled_y, _ = shuffle_data(data_x, data_y)
-    data_size = shuffled_x.shape[0]
-    train_data_ratio = 0.8    # TODO: 0.9
-    train_data_len = round(train_data_ratio*data_size)
-    train_x = shuffled_x[:train_data_len, :, :]
-    train_y = shuffled_y[:train_data_len, :]
-    test_x = shuffled_x[train_data_len:, :, :]
-    test_y = shuffled_y[train_data_len:, :]
-
-    train_dict = {'data': train_x, 'label': train_y}
-    test_dict = {'data': test_x, 'label': test_y}
-    np.save(train_dir, train_dict)
-    np.save(test_dir, test_dict)
-    print(f"Data saved to {train_dir}, {test_dir}")
-
-
-def make_continuous_data(data_path, train_dir, test_dir):
-    """
+def prep_data(data_path, train_dir, test_dir, is_discrete:bool):
+    """ Prepare data for training and testing\n
+        Parameters:\n
+            `data_path`: path for preprocessed data;\n
+            `train_dir`: training set directory;\n
+            `test_dir`: testing set directory;\n
+            `is_discrete`: if the input data is discrete
     """
     data = loadmat(data_path)['Doppler']
     label = loadmat(data_path)['Label']
@@ -55,13 +25,22 @@ def make_continuous_data(data_path, train_dir, test_dir):
         seq_len, feat_dims = raw_x.shape
         dop_x = np.abs(raw_x) # extract doppler
         raw_y = np.zeros([seq_len, 1], dtype=np.int32)
-        raw_y[:, :] = label[i][0].T - 1 # convert the indexing format
+        # convert the indexing format of labels
+        if is_discrete == True:
+            raw_y[:, :] = label[i][0] - 1
+        else:
+            raw_y[:, :] = label[i][0].T - 1
         slide_x, slide_y = sliding_window(dop_x, raw_y, overlap=0.9, win_len=50)
         data_x.append(slide_x)
         data_y.append(slide_y)
     data_x = np.vstack(data_x)
     data_y = np.vstack(data_y) # (data_size, seq_len, feat_dims)
 
+
+def data_saver(data_x, data_y, train_dir, test_dir):
+    """
+        Do the train test split and save data and labels into dictionaries
+    """
     # train test split
     shuffled_x, shuffled_y, _ = shuffle_data(data_x, data_y)
     data_size = shuffled_x.shape[0]
@@ -71,13 +50,24 @@ def make_continuous_data(data_path, train_dir, test_dir):
     train_y = shuffled_y[:train_data_len, :]
     test_x = shuffled_x[train_data_len:, :, :]
     test_y = shuffled_y[train_data_len:, :]
-
+    # save as dictionary
     train_dict = {'data': train_x, 'label': train_y}
     test_dict = {'data': test_x, 'label': test_y}
     np.save(train_dir, train_dict)
     np.save(test_dir, test_dict)
     print(f"Data saved to {train_dir}, {test_dir}")
+
+
+def main():
+    data_dir = 'datasets/processed/'
+    filenames = os.listdir(data_dir)
+    for filename in filenames:
+        if filename == 'Continuous1_48.mat':
+            data_x, data_y = prep_data('datasets/processed/Continuous1_48.mat', 'data/Doppler_train.npy', 'data/Doppler_test.npy', False)
+        else:
+            data_x, data_y = prep_data('datasets/processed/Discrete1_360.mat', 'data/Doppler_train.npy', 'data/Doppler_test.npy', True)
+    
+    data_saver(data_x, data_y, train_dir, test_dir)
 
 if __name__ == "__main__":
-    # make_discrete_data('datasets/processed/discrete2.mat', 'data/Doppler_train.npy', 'data/Doppler_test.npy')
-    make_continuous_data('datasets/processed/continuous.mat', 'data/Doppler_train.npy', 'data/Doppler_test.npy')
+    main()
